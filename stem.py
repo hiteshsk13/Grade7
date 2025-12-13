2bc1da5add626b08656b76244bbfb9a3b3108ab1
hiteshsk13 <hitesh.sk13@gmail.com>
2025-12-13 11:57:39 -0500

    Update stem.py

--- stem.py
+++ stem.py
@@ -13,7 +13,7 @@ st.set_page_config(
 
 st.title("Crop Guard AI")
 st.markdown("""
-<b><u>About:</b><br> This is built to detect pesticides, wax, rot and ripe.<br> This was made by Hitesh, Arindam, and Nidhaan. works on phone, computer, ipad, and everything eles</u>
+<b><u>About:</b><br> This is built to detect pesticides, wax, rot and ripe.<br> This was made by Hitesh, Arindam, and Nidhaan. Works on phone, computer, ipad, and everything eles</u>
 """, unsafe_allow_html=True)
 
 
@@ -181,6 +181,7 @@ avredge each picture then show outcome
     # shows the AI result
     st.write("AI Analysis:")
     st.markdown(response.text)
+
 
 
 
